56731dc3233df6fccfb71edab1e931e0b910719f
jaeyeong97 <rkaakdbdb1822@naver.com>
2024-03-09 19:32:55 +0900

    로딩페이지 수정

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer id="footer">
             <div className='footer_in'>
-                
+                <span>© 2024 JaeYeongJang All rights reserved.</span>
                 <Link to="nav_home" smooth={true}>
                     <div className='go_top'>
                         <span className="material-symbols-outlined arrowsTop">keyboard_double_arrow_up</span>
